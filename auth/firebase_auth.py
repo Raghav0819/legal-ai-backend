6d80012956d92ae8b264f36c2c1de88b15dd2c5c
Raghav0819 <raghav.khandelwal.23cse@bmu.edu.in>
2026-05-29 18:19:08 +0530

    change in firebase auth.

--- auth/firebase_auth.py
+++ auth/firebase_auth.py
@@ -32,11 +32,13 @@ firebase_credentials = {
         ),
 
     "private_key":
+    (
         os.getenv(
             "FIREBASE_PRIVATE_KEY"
         ).replace(
             "\\n",
             "\n"
+        )
         ),
 
     "client_email":
@@ -47,6 +49,19 @@ firebase_credentials = {
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
 
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+
+    "auth_uri":
+        os.getenv("FIREBASE_AUTH_URI"),
+
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+
+    "universe_domain":        os.getenv(
+            "FIREBASE_UNIVERSE_DOMAIN"
+        ),
 }
 
 cred = credentials.Certificate(
